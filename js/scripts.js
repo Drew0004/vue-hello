1afbf2dcd0481ca724e21331feb83c72a0106aa6
Drew0004 <drdrewgraphic@gmail.com>
2023-12-22 15:13:37 +0100

    Aggiunte classi

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,13 @@ createApp({
     data() {
         return {
             message: 'Buon Natale con VueJs!',
-            url: 'img/Meme-Alessio.jpg'
+            url: 'img/Meme-Alessio.jpg',
+            textClass: 'text-center my-4'
+        }
+    },
+    methods: {
+        createImgClass(){
+            return 'w-100 d-block';
         }
     }
 }).mount('#app');
