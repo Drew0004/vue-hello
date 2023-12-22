ce7943515f1cdf50997eb054066881151302eab8
Drew0004 <drdrewgraphic@gmail.com>
2023-12-22 14:58:54 +0100

    Aggiunto titolo e bonus

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'Buon Natale con VueJs!',
+            url: 'img/Meme-Alessio.jpg'
+        }
+    }
+}).mount('#app');
